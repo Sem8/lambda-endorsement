c171023198ab278c0e504174901bdc8b54fefa94
Sem8 <mssemmi8@gmail.com>
2020-01-08 16:46:55 -0500

    wrote some intiial react tests to see that the main App componenet got rendered to the dom and displays some expected text

---
+++ Frontend/Testing/project/src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="App">
+      <span className='greet'>Hello World</span>
+    </div>
+  );
+}
+
+export default App;
---
+++ Frontend/Testing/project/src/App.test.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { render } from "@testing-library/react";
+import * as rtl from "@testing-library/react";
+import "@testing-library/jest-dom";
+import App from "./App";
+
+test("renders a span with the text Hello World", () => {
+  const wrapper = rtl.render(<App />);
+  // console.log(wrapper.debug());
+  const element = wrapper.queryByText(/hello world/i);
+  expect(element).toBeInTheDocument();
+});
