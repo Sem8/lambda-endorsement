02d355b444a8d9e63556f5669c545db921cbe03e
Sem8 <mssemmi8@gmail.com>
2020-01-08 18:40:55 -0500

    wrote some tests for testing async code with promises

--- Frontend/Testing/testingWebApp/src/helpers.js
+++ Frontend/Testing/testingWebApp/src/helpers.js
@@ -1,10 +1,21 @@
-exports.sum = function(...args) {
-  return args.reduce((accumulator, prev) => {
-    return accumulator + prev;
-  });
-};
+// exports.sum = function(...args) {
+//   return args.reduce((accumulator, prev) => {
+//     return accumulator + prev;
+//   });
+// };
 
 // const sum = (...args) => {
 //   return args;
 // };
 // console.log(sum(1, 2, 3));  // [1, 2, 3] - spread operator returns an array of parameters
+
+
+/* Testing part II */
+exports.asyncThing = (callback) => {
+  return new Promise(resolve => {
+    setTimeout(() => {
+      callback();
+      resolve(7);
+    }, 1000)
+  });
+}
--- Frontend/Testing/testingWebApp/src/helpers.test.js
+++ Frontend/Testing/testingWebApp/src/helpers.test.js
@@ -1,20 +1,37 @@
 const help = require("./helpers");
 
-describe("helpers mdoule", () => {
-  describe("sum function", () => {
-    it("can add two numbers", () => {
-      const expectedOutput = 7;
-      const actualOutput = help.sum(2, 5);
-      expect(actualOutput).toBe(expectedOutput);
-      //   expect(actualOutput).not.toBe(10);
-      //   expect(actualOutput).toBeTruthy();
-      //   expect(actualOutput).toBeGreaterThan(3);
-    });
+// describe("helpers mdoule", () => {
+//   describe("sum function", () => {
+//     it("can add two numbers", () => {
+//       const expectedOutput = 7;
+//       const actualOutput = help.sum(2, 5);
+//       expect(actualOutput).toBe(expectedOutput);
+//       //   expect(actualOutput).not.toBe(10);
+//       //   expect(actualOutput).toBeTruthy();
+//       //   expect(actualOutput).toBeGreaterThan(3);
+//     });
 
-    it("can add three numbers", () => {
-      const expectedOutput = 7;
-      const actualOutput = help.sum(2, 3, 2);
-      expect(actualOutput).toBe(expectedOutput);
-    });
+//     it("can add three numbers", () => {
+//       const expectedOutput = 7;
+//       const actualOutput = help.sum(2, 3, 2);
+//       expect(actualOutput).toBe(expectedOutput);
+//     });
+//   });
+// });
+
+/* Testing part II */
+describe("asyncThing function", () => {
+  it("resolves to 7 eventually", async () => {
+    const promise = help.asyncThing(Function.prototype);
+    const resolved = await promise;
+    expect(resolved).toBe(7);
+  });
+
+  it("invokes the callback eventually", async () => {
+    const spy = jest.fn();
+    expect(spy).not.toBeCalled();
+    const promise = await help.asyncThing(spy);
+    // await promise;
+    expect(spy).toBeCalled();
   });
 });
