9cd989604d75d91c0b8d260aaaf85b63b86eec77
Sem8 <mssemmi8@gmail.com>
2020-01-08 17:06:02 -0500

    wrote some tests in react to get text content from an element using textContent method and get the rendered DOM component from function component using the render method from react testing library

--- Frontend/Testing/project/src/App.js
+++ Frontend/Testing/project/src/App.js
@@ -1,11 +1,19 @@
 import React from "react";
 
-function App() {
+// function App() {
+//   return (
+//     <div className="App">
+//       <span className='greet'>Hello World</span>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+export default function App() {
   return (
-    <div className="App">
-      <span className='greet'>Hello World</span>
+    <div className="container">
+      <h1 className="heading">Lambda</h1>
     </div>
   );
 }
-
-export default App;
--- Frontend/Testing/project/src/App.test.js
+++ Frontend/Testing/project/src/App.test.js
@@ -4,11 +4,21 @@ import * as rtl from "@testing-library/react";
 import "@testing-library/jest-dom";
 import App from "./App";
 
-test("renders a span with the text Hello World", () => {
-  const wrapper = rtl.render(<App />);
-  // console.log(wrapper.debug());
-  const element = wrapper.queryByText(/hello world/i);
-  expect(element).toBeInTheDocument();
-  expect(element).toBeTruthy();
-  expect(element).toBeVisible();
+// test("renders a span with the text Hello World", () => {
+//   const wrapper = rtl.render(<App />);
+//   // console.log(wrapper.debug());
+//   const element = wrapper.queryByText(/hello world/i);
+//   expect(element).toBeInTheDocument();
+//   expect(element).toBeTruthy();
+//   expect(element).toBeVisible();
+// });
+
+describe("App", () => {
+  it("mounts to a kind of simulated DOM", () => {
+    const simulatedDOM = rtl.render(<App />);
+    // console.log(simulatedDOM.debug());
+    const h1 = simulatedDOM.queryByText(/lambda/i);
+    // console.log(h1);
+    console.log(h1.textContent);
+  });
 });
