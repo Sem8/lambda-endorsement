b2205d4a4c384bdc70517e84c2c907c8f513d540
Sem8 <mssemmi8@gmail.com>
2020-01-08 20:02:43 -0500

    wrote some tests for mocks and spies

--- Frontend/Testing/testingWebApp/src/helpers.js
+++ Frontend/Testing/testingWebApp/src/helpers.js
@@ -1,21 +1,36 @@
-// exports.sum = function(...args) {
-//   return args.reduce((accumulator, prev) => {
-//     return accumulator + prev;
-//   });
-// };
+const uuid = require("uuid");
+
+exports.sum = function(...args) {
+  return args.reduce((accumulator, prev) => {
+    return accumulator + prev;
+  });
+};
 
 // const sum = (...args) => {
 //   return args;
 // };
 // console.log(sum(1, 2, 3));  // [1, 2, 3] - spread operator returns an array of parameters
 
+/* Testing part II - testing Async code */
+// exports.asyncThing = (callback) => {
+//   return new Promise(resolve => {
+//     setTimeout(() => {
+//       callback();
+//       resolve(7);
+//     }, 1000)
+//   });
+// }
 
-/* Testing part II */
-exports.asyncThing = (callback) => {
-  return new Promise(resolve => {
-    setTimeout(() => {
-      callback();
-      resolve(7);
-    }, 1000)
-  });
-}
+/* Testing  part III - testing with mocks and spies*/
+exports.makePerson = (firstName, lastName) => {
+  return {
+    id: uuid(),
+    fullName: `${firstName} ${lastName}`
+  };
+};
+
+exports.executeIfEven = function(number, callback) {
+  if (number % 2 === 0) {
+    callback(number);
+  }
+};
--- Frontend/Testing/testingWebApp/src/helpers.test.js
+++ Frontend/Testing/testingWebApp/src/helpers.test.js
@@ -1,7 +1,7 @@
 const help = require("./helpers");
 
 jest.useFakeTimers();
-jest.mock("uuid", () => "123");
+jest.mock("uuid", () => () => "123");
 
 // describe("helpers mdoule", () => {
 //   describe("sum function", () => {
@@ -23,20 +23,43 @@ jest.mock("uuid", () => "123");
 // });
 
 /* Testing part II - Testing async code */
-describe("asyncThing function", () => {
-  it("resolves to 7 eventually", async () => {
-    const promise = help.asyncThing(Function.prototype);
-    jest.runAllTimers(); // makes running tests faster when waiting for async code, write it before the await
-    const resolved = await promise;
-    expect(resolved).toBe(7);
+// describe("asyncThing function", () => {
+//   it("resolves to 7 eventually", async () => {
+//     const promise = help.asyncThing(Function.prototype);
+//     jest.runAllTimers(); // makes running tests faster when waiting for async code, write it before the await
+//     const resolved = await promise;
+//     expect(resolved).toBe(7);
+//   });
+
+//   it("invokes the callback eventually", async () => {
+//     const spy = jest.fn();
+//     expect(spy).not.toBeCalled();
+//     const promise = help.asyncThing(spy);
+//     jest.runAllTimers(); // makes running tests faster when waiting for async code, write it before the await
+//     await promise;
+//     expect(spy).toBeCalled();
+//   });
+// });
+
+/* Testing  part III - testing with mocks and spies*/
+// describe("makePerson function", () => {
+//   it("can create a person from firstName and lastName", () => {
+//     const expected = { id: "123", fullName: "John Smith" };
+//     const actual = help.makePerson("John", "Smith");
+//     expect(actual).toEqual(expected);
+//   });
+// });
+
+describe("executeIfEven", () => {
+  it("invokes callback if passed an even num", () => {
+    const spy = jest.fn();
+    help.executeIfEven(2, spy);
+    expect(spy).toBeCalledWith(2);
   });
 
-  it("invokes the callback eventually", async () => {
+  it("does NOT invoke callback if passed an odd number", () => {
     const spy = jest.fn();
+    help.executeIfEven(3, spy);
     expect(spy).not.toBeCalled();
-    const promise = help.asyncThing(spy);
-    jest.runAllTimers(); // makes running tests faster when waiting for async code, write it before the await
-    await promise;
-    expect(spy).toBeCalled();
   });
 });
