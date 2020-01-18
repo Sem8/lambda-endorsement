fbe9e0f3e70fb0edec7794ef5ca470271c0749a9
Sem8 <mssemmi8@gmail.com>
2020-01-17 19:28:25 -0500

    created functionality for getting dynamic data from request query parameters and requery strings to manipulate our server data from client for delete and get requests

--- Backend/node_express_intro_tk/hello-world-express/server.js
+++ Backend/node_express_intro_tk/hello-world-express/server.js
@@ -10,9 +10,57 @@ const port = 5000;
 // });
 
 const server = express();
+server.use(express.json());
 
-server.get('/', (req, res) => {
-    res.send('Hello World from Express!');
+// server.get('/', (req, res) => {
+//     res.send('Hello World from Express!');
+// });
+
+// server.get('/hobbits', (req, res) => {
+//     res.send('Welcome to Hobbiton');
+// });
+
+server.get('/hobbits', (req, res) => {
+    console.log(req.query);
+    const sortField = req.query.sortBy || 'id';
+    const hobbits = [
+        {
+            id: 1,
+            name: 'Samwise Gamgee'
+        },
+        {
+            id: 3,
+            name: 'Bilboa Baggins',
+        },
+        {
+            id: 2,
+            name: 'Frodo Baggins',
+        }
+    ];
+    
+    const response = hobbits.sort(
+        (a, b) => (a[sortField] < b[sortField] ? -1 : 1)
+      );
+
+    res.status(200).json(response);
+});
+
+server.post('/hobbits', (req, res) => {
+    res.status(201).json({ url: '/hobbits', operation: 'POST'});
+});
+
+server.put('/hobbits', (req, res) => {
+    res.status(200).json({ url: '/hobbits', operation: 'PUT'});
+});
+
+server.delete('/hobbits/:id', (req, res) => {
+    // res.status(204);
+    const id = req.params.id;
+    console.log(req.params);
+    res.status(200).json({
+        url: `/hobbits/${id}`,
+        operation: `DELETE for hibbit with id ${id}`,
+    });
 });
 
 server.listen(port, () => {
